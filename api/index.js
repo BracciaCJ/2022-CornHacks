3fe23db89df6d64177d30f3c29c65d441f7e123d
Danny Tran <danny.tran2018@gmail.com>
2022-01-22 18:36:28 -0600

    Updated function to correctly pass analysis as a JSON.

--- api/index.js
+++ api/index.js
@@ -15,56 +15,49 @@ app.use(bodyParser.json());
 app.get('/', async function(req, res) {
     // Access the provided 'url' query parameters
     let url = req.query.url;
-    console.log(url);
     if (url){
-    pa11y(url, {runners:['axe']}).then((results) => {
-        // Diasplay the number of issues found to the user as JSON
-        let axeCodes = new Set();
-        results.issues.forEach(element => {
-            axeCodes.add(element.code);
-        });
-        AnalyzeResult(axeCodes);
-}).catch(()=>{
-    res.json({error:`Could not process ${url}`});
+        pa11y(url, {runners:['axe']}).then(async (results) => {
+            // Display Violations and total Score
+            let axeCodes = new Set();
 
-});}
-else{res.json({error:"Failed to read URL from string"});}
+            results.issues.forEach(element => {
+                axeCodes.add(element.code);
+            });
+        
+            var jsonObj = {
+                "totalScore": 100,
+                "violations": []
+            }
+
+            fs.createReadStream('AxeCoreCode.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+                if (axeCodes.has(row['Rule ID'])){
+                    jsonObj['violations'].push(row['Rule ID']);
+                    if (row['blind']){
+                        jsonObj['totalScore'] += parseInt(row['blind']);
+                    }
+                    if (row['colorblind']){
+                        jsonObj['totalScore'] += parseInt(row['colorblind'])
+                    }
+                    if (row['deaf']){
+                        jsonObj['totalScore'] += parseInt(row['deaf']);
+                    }
+                    if (row['deafblind']){ 
+                        jsonObj['totalScore'] += parseInt(row['deafblind']);
+                    }
+                }
+            })
+            .on('end', () => {
+                res.json(jsonObj)
+            });
+        }).catch(()=>{
+            res.json({error:`Could not process ${url}`});
+        });
+    } else {
+        res.json({error:"Failed to read URL from string"});
+    }
 });
 let server = app.listen(8080, function() {
     console.log('Server is listening on port 8080');
 });
-
-
-function AnalyzeResult(axeCodeSet){
-    console.log(axeCodeSet);
-    let totalScore = 100;
-    fs.createReadStream('AxeCoreCode.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        if (axeCodeSet.has(row['Rule ID'])){
-            console.log("------------------------")
-            console.log("Rule ID: " + row['Rule ID'])
-            // console.log(row['blind']);
-            // console.log(row['colorblind']);
-            // console.log(row['deaf']);
-            // console.log(row['deafblind']);
-
-            if (row['blind']){
-                totalScore += parseInt(row['blind'])
-            }
-            if (row['colorblind']){
-                totalScore += parseInt(row['colorblind'])
-            }
-            if (row['deaf']){
-                totalScore += parseInt(row['deaf'])
-            }
-            if (row['deafblind']){
-                totalScore += parseInt(row['deafblind'])
-            }
-            console.log("Total Score: " + totalScore)
-        }
-    })
-    .on('end', () => {
-        console.log('Website successfully analyzed');
-    });
-  };
