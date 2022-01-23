96ecb5b6cc0d2f46ae88c1ba35d110e0fc3f495f
Emma Hubka <34105895+emma-hubka@users.noreply.github.com>
2022-01-23 03:11:37 -0600

    Added support for top stories.

--- extension/background.js
+++ extension/background.js
@@ -19,75 +19,139 @@ async function getA11yScores() {
       },
       function (items) {
         // Update the ending based on the stored values
-        ending = ending + 'b' + (items.blind ? 't' : 'f');
-        ending = ending + 'd' + (items.deaf ? 't' : 'f');
-        ending = ending + 'c' + (items.colorBlind ? 't' : 'f');
-        ending = ending + 'a' + (items.add ? 't' : 'f');
-        ending = ending + 'l' + (items.lv ? 't' : 'f');
-        ending = ending + 'm' + (items.motor ? 't' : 'f');
+        ending = ending + "b" + (items.blind ? "t" : "f");
+        ending = ending + "d" + (items.deaf ? "t" : "f");
+        ending = ending + "c" + (items.colorBlind ? "t" : "f");
+        ending = ending + "a" + (items.add ? "t" : "f");
+        ending = ending + "l" + (items.lv ? "t" : "f");
+        ending = ending + "m" + (items.motor ? "t" : "f");
         //doing g for cognitive because c is taken
-        ending = ending + 'g' + (items.cognitive ? 't' : 'f');
-        ending = ending + 's' + (items.sk ? 't' : 'f');
-        
+        ending = ending + "g" + (items.cognitive ? "t" : "f");
+        ending = ending + "s" + (items.sk ? "t" : "f");
+
         // Iterate through all links
         for (var linkElement of linkElements) {
           // Adds a container with text score loading as the first child in each of the containers.
-          let gContainer = linkElement.parentNode.parentNode.parentNode.parentNode;
-          let gContainerTwo = linkElement.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-          console.log(linkElement);
-          if (!linkElement.hasAttribute('role') && (linkElement.classList.contains('l') || linkElement.classList.contains('fl') || gContainer.classList.contains('g') || gContainerTwo.classList.contains('g') || gContainerTwo.tagName.toLowerCase() == 'li') && !linkElement.getAttribute('href').startsWith('/search')) {
+          let gContainer =
+            linkElement.parentNode.parentNode.parentNode.parentNode;
+          let gContainerTwo =
+            linkElement.parentNode.parentNode.parentNode.parentNode.parentNode
+              .parentNode;
+          // console.log(linkElement);
+          if (
+            !linkElement.hasAttribute("role") &&
+            (linkElement.classList.contains("l") ||
+              linkElement.classList.contains("fl") ||
+              linkElement.classList.contains("WlydOe") ||
+              gContainer.classList.contains("g") ||
+              gContainerTwo.classList.contains("g") ||
+              gContainerTwo.tagName.toLowerCase() == "li") &&
+            !linkElement.getAttribute("href").startsWith("/search")
+          ) {
             // Pass each of the URLs to be scanned by the API
             // NOTE: We encode the URI component so the special characters do not break the link.
-            let apiUrl = `http://localhost:8080?url=${encodeURIComponent(linkElement.href)}&disabilities=${ending}`
-            
-
-            if (linkElement.classList.contains('l')||linkElement.classList.contains('fl')) {
-              let storedLinkElement = linkElement;
-              let originalHTML = linkElement.innerHTML;
-
-              storedLinkElement.innerHTML = "Ha11y Loading - " + originalHTML;
-                fetch(apiUrl).then((results) => {
-                  return results.json();
-                }).then(json => {
-                  if (json.totalScore) {
-                    // Temporary hack for displaying the results from the API
-                    storedLinkElement.innerHTML = `Ha11y Score ${json.totalScore} - ` + originalHTML;
-                  } else {
-                    // This should never happen but we should be prepared to handle this.
-                    storedLinkElement.innerHTML = `Ha11y Error ` + originalHTML + ` - Ha11y error was ${json.errors}`;
-                  }
-                }).catch((error) => {
-                  console.log(error);
-                  storedLinkElement.innerHTML = 'Ha11y Error ' + originalHTML + ` - Ha11y Server Error was ${error.toString()}`;
-                });
-            } else if (linkElement.hasAttribute("data-ved")){
-              let linkText = linkElement.getElementsByTagName('h3')[0];
+            let apiUrl = `http://localhost:8080?url=${encodeURIComponent(
+              linkElement.href
+            )}&disabilities=${ending}`;
+            if (linkElement.classList.contains("WlydOe")) {
+              let linkText = linkElement.getElementsByClassName("mCBkyc")[0];
               let originalHTML = linkText.innerHTML;
               linkText.innerHTML = "Ha11y Loading - " + originalHTML;
               // The following line should be considered make the app messier rather than more beneficial
               //linkText.setAttribute("aria-live","polite");
 
-
-                fetch(apiUrl).then((results) => {
+              fetch(apiUrl)
+                .then((results) => {
                   return results.json();
-                }).then(json => {
+                })
+                .then((json) => {
                   if (json.totalScore) {
                     // Temporary hack for displaying the results from the API
-                    linkText.innerHTML = `Ha11y Score ${json.totalScore} - ` + originalHTML;
+                    linkText.innerHTML =
+                      `Ha11y Score ${json.totalScore} - ` + originalHTML;
                   } else {
                     // This should never happen but we should be prepared to handle this.
-                    linkText.innerHTML = `Ha11y Error ` + originalHTML + ` - Ha11y error was ${json.errors}`;
+                    linkText.innerHTML =
+                      `Ha11y Error ` +
+                      originalHTML +
+                      ` - Ha11y error was ${json.errors}`;
                   }
-                }).catch((error) => {
-                  console.log(error);
-                  linkText.innerHTML = 'Ha11y Error ' + originalHTML + ` - Ha11y Server Error was ${error.toString()}`;
+                })
+                .catch((error) => {
+                  //console.log(error);
+                  linkText.innerHTML =
+                    "Ha11y Error " +
+                    originalHTML +
+                    ` - Ha11y Server Error was ${error.toString()}`;
                 });
-              
+            } else if (
+              linkElement.classList.contains("l") ||
+              linkElement.classList.contains("fl")
+            ) {
+              let storedLinkElement = linkElement;
+              let originalHTML = linkElement.innerHTML;
+
+              storedLinkElement.innerHTML = "Ha11y Loading - " + originalHTML;
+              fetch(apiUrl)
+                .then((results) => {
+                  return results.json();
+                })
+                .then((json) => {
+                  if (json.totalScore) {
+                    // Temporary hack for displaying the results from the API
+                    storedLinkElement.innerHTML =
+                      `Ha11y Score ${json.totalScore} - ` + originalHTML;
+                  } else {
+                    // This should never happen but we should be prepared to handle this.
+                    storedLinkElement.innerHTML =
+                      `Ha11y Error ` +
+                      originalHTML +
+                      ` - Ha11y error was ${json.errors}`;
+                  }
+                })
+                .catch((error) => {
+                  //console.log(error);
+                  storedLinkElement.innerHTML =
+                    "Ha11y Error " +
+                    originalHTML +
+                    ` - Ha11y Server Error was ${error.toString()}`;
+                });
+            } else if (linkElement.hasAttribute("data-ved")) {
+              let linkText = linkElement.getElementsByTagName("h3")[0];
+              let originalHTML = linkText.innerHTML;
+              linkText.innerHTML = "Ha11y Loading - " + originalHTML;
+              // The following line should be considered make the app messier rather than more beneficial
+              //linkText.setAttribute("aria-live","polite");
+
+              fetch(apiUrl)
+                .then((results) => {
+                  return results.json();
+                })
+                .then((json) => {
+                  if (json.totalScore) {
+                    // Temporary hack for displaying the results from the API
+                    linkText.innerHTML =
+                      `Ha11y Score ${json.totalScore} - ` + originalHTML;
+                  } else {
+                    // This should never happen but we should be prepared to handle this.
+                    linkText.innerHTML =
+                      `Ha11y Error ` +
+                      originalHTML +
+                      ` - Ha11y error was ${json.errors}`;
+                  }
+                })
+                .catch((error) => {
+                  //console.log(error);
+                  linkText.innerHTML =
+                    "Ha11y Error " +
+                    originalHTML +
+                    ` - Ha11y Server Error was ${error.toString()}`;
+                });
             }
           }
         }
-
-      });
+      }
+    );
   }
 }
 
@@ -96,12 +160,12 @@ chrome.webRequest.onCompleted.addListener(
     // Run the get A11y Scores function after google searching.
     // Do not run on fromCache as it crashes due to lack of permissions
     // This occurs when you click on a Google Result then go back a page
-    if(!details['fromCache']){
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      function: getA11yScores,
-    });
-  }
+    if (!details["fromCache"]) {
+      chrome.scripting.executeScript({
+        target: { tabId: details.tabId },
+        function: getA11yScores,
+      });
+    }
   },
   { urls: [`*:\/\/*.google.com/search?q=*`] }
 );
